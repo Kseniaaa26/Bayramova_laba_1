3a0cd2d5488e682622d846c45d1680027f76d510
Kseniaaa26 <ks.br2014@mail.ru>
2023-09-27 12:16:21 +0300

    Описание свойств трубы и КС

---
+++ Bayramova_laba_1/Bayramova_laba_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pipe {
+	string name_pipe;
+	double length;
+	int diameter;
+	bool in_repairing;
+};
+
+struct CS {
+	string name_CS;
+	int number_of_workshops;
+	int workshops_in_operation;
+	string effectiveness;
+}
