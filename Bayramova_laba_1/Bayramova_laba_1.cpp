9bf06c785a0f8e59711fef43201bcfaa10df2500
Kseniaaa26 <ks.br2014@mail.ru>
2023-09-27 12:42:47 +0300

    Считывание с консоли с проверкой вводимых значений

--- Bayramova_laba_1/Bayramova_laba_1.cpp
+++ Bayramova_laba_1/Bayramova_laba_1.cpp
@@ -3,16 +3,84 @@
 
 using namespace std;
 
+inline int CorrectNumber(int min, int max) {
+    int x;
+    while ((cin >> x).fail()
+        || cin.peek() != '\n'
+        || x < min || x > max)
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "\nНеверный ввод данных!" << endl;
+        cout << "Введите число от " << min << "до " << max << endl;
+    }
+    return x;
+}
+
+inline double CorrectNumber(double min, double max) {
+    double x;
+    while ((cin >> x).fail()
+        || cin.peek() != '\n'
+        || x < min || x > max)
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "\nНеверный ввод данных!" << endl;
+        cout << "Введите число от " << min << "до " << max << endl;
+    }
+    return x;
+}
+
 struct Pipe {
-	string name_pipe;
-	double length;
-	int diameter;
-	bool in_repairing;
+    string name_p;
+    double length;
+    int diameter;
+    bool in_repairing;
 };
 
 struct CS {
-	string name_CS;
-	int number_of_workshops;
-	int workshops_in_operation;
-	string effectiveness;
+    string name_CS;
+    int number_of_workshops;
+    int workshops_in_operation;
+    string effectiveness;
+};
+
+void inputPipeInfo(Pipe& pipe) {
+
+    cout << "Введите название трубы: ";
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    getline(cin, pipe.name_p);
+
+    cout << "Введите длину трубы: ";
+    pipe.length = CorrectNumber(0.0, DBL_MAX);
+
+    cout << "Введите диаметр трубы: ";
+    pipe.diameter = CorrectNumber(0, INT_MAX);
+    
+    cout << "Введите состояние трубы(0 - в ремонте, 1 - в работе):" << pipe.in_repairing << endl;
+    if (CorrectNumber(0, 1)) {
+        pipe.in_repairing = true;
+    }
+    else {
+        pipe.in_repairing = false;
+    }
 }
+
+void inputCSInfo(CS& cs) {
+
+    cout << "Введите название КС: ";
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    getline(cin, cs.name_CS);
+
+    cout << "Введите количество цехов КС: ";
+    cs.number_of_workshops = CorrectNumber(0, 50);
+
+    cout << "Введите количество цехов в работе: ";
+    cs.workshops_in_operation = CorrectNumber(0, cs.number_of_workshops);
+
+    cout << "Укажите эффективность(1,2,3)" << endl;
+    cs.effectiveness = CorrectNumber(1, 3);
+}
+
